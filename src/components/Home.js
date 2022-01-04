3c14adc528f80954be97c846bc728db9d1b1e59d
Brian Bellinger <brianpatrickbellinger@gmail.com>
2022-01-03 19:42:31 -0800

    wip

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,9 +2,9 @@ import React from "react";
 
 const Accommodations = props => {
     return (
-        <div>
+        <div className='home'>
             <h1> On the North Fork </h1>
-            <img src='https://i.ibb.co/kDq1hVG/nf-banner.jpg' alt='nf-banner' border='0' />
+            <img src='https://i.ibb.co/kDq1hVG/nf-banner.jpg' alt='nf-banner'/>
         </div>
     )
 }
