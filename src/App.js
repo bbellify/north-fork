0901ac80643759663dd06f3f0403512c934e6680
Brian Bellinger <brianpatrickbellinger@gmail.com>
2022-01-03 18:21:27 -0800

    more set up - routes, css reset, more

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import './App.css';
+import { NavLink, Route, Routes } from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
+      <nav>
+        <NavLink to='/specials'>Specials</NavLink>
+        <NavLink to='/'>Home</NavLink>
+        <NavLink to='/accommodations'>Accommodations</NavLink>
+        <NavLink to='/weddings'>Weddings</NavLink>
+        <NavLink to='/gatherings'>Gatherings</NavLink>
+        <NavLink to='/meetings'>Meetings</NavLink>
+        <NavLink to='/maps'>Maps</NavLink>
+      </nav>
       <h1> On the North Fork </h1>
+      <img src='https://i.ibb.co/2nzRS6P/IMG-0938.jpg' alt='horsies' width='50%' />
     </div>
   );
 }
